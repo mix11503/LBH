04aa90d31cc9b15e86f40829266fb92d1478a52a
mix11503 <mix11503@gmail.com>
2017-11-12 19:34:30 +0700

    News Noti

--- src/java/Controller/News/createNews.java
+++ src/java/Controller/News/createNews.java
@@ -8,7 +8,11 @@ package Controller.News;
 import Model.newsUpdate;
 import static Model.notifyUser.createNotiNews;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -34,17 +38,18 @@ public class createNews extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
-        
+
         String topic = request.getParameter("topic");
         String desc = request.getParameter("desc");
         String start = request.getParameter("startdate");
         String end = request.getParameter("enddate");
         String cate = request.getParameter("category");
         String adName = request.getParameter("adName");
-        String att = "<h6 style=\"text-align: right;\"><em>By Admin: "+adName+"</em></h6>";
+        String att = "<h6 style=\"text-align: right;\"><em>By Admin: " + adName + "</em></h6>";
         newsUpdate nu = null;
         String message = null;
-        
+        String notiFilters = "";
+
         String all = request.getParameter("0");
         String f2 = request.getParameter("200");
         String f3 = request.getParameter("300");
@@ -53,33 +58,111 @@ public class createNews extends HttpServlet {
         String f6 = request.getParameter("600");
         String f7 = request.getParameter("700");
         String f8 = request.getParameter("800");
-        
-        try{
+
+        try {
             nu = new newsUpdate();
             nu.setNews_topic(topic);
-            nu.setNews_desc(desc+" "+att);
+            nu.setNews_desc(desc + " " + att);
             nu.setCate(cate);
             nu.createNews(start, end);
-            
-            if(all!=null){
-             nu.attachedTag(nu.getNews_id(), Integer.parseInt(all));
-             createNotiNews("",nu.getNews_id(),0);
-            }else{
-            if(f2!=null){nu.attachedTag(nu.getNews_id(), Integer.parseInt(f2));createNotiNews("",nu.getNews_id(),200);}
-            if(f3!=null){nu.attachedTag(nu.getNews_id(), Integer.parseInt(f3));createNotiNews("",nu.getNews_id(),300);}
-            if(f4!=null){nu.attachedTag(nu.getNews_id(), Integer.parseInt(f4));createNotiNews("",nu.getNews_id(),400);}
-            if(f5!=null){nu.attachedTag(nu.getNews_id(), Integer.parseInt(f5));createNotiNews("",nu.getNews_id(),500);}
-            if(f6!=null){nu.attachedTag(nu.getNews_id(), Integer.parseInt(f6));createNotiNews("",nu.getNews_id(),600);}
-            if(f7!=null){nu.attachedTag(nu.getNews_id(), Integer.parseInt(f7));createNotiNews("",nu.getNews_id(),700);}
-            if(f8!=null){nu.attachedTag(nu.getNews_id(), Integer.parseInt(f8));createNotiNews("",nu.getNews_id(),800);}
-            }     
+
+            if (all != null) {
+                nu.attachedTag(nu.getNews_id(), Integer.parseInt(all));
+                createNotiNews("", nu.getNews_id(), 0);
+                notiFilters += "\"included_segments\": [\"All\"],";
+            } else {
+                 notiFilters += "\"filters\": [{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"0\"}";
+                if (f2 != null) {
+                    nu.attachedTag(nu.getNews_id(), Integer.parseInt(f2));
+                    createNotiNews("", nu.getNews_id(), 200);
+                    notiFilters += ",{\"operator\": \"OR\"},{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"2\"}";
+                }
+                if (f3 != null) {
+                    nu.attachedTag(nu.getNews_id(), Integer.parseInt(f3));
+                    createNotiNews("", nu.getNews_id(), 300);
+                    notiFilters += ",{\"operator\": \"OR\"},{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"3\"}";
+                }
+                if (f4 != null) {
+                    nu.attachedTag(nu.getNews_id(), Integer.parseInt(f4));
+                    createNotiNews("", nu.getNews_id(), 400);
+                    notiFilters += ",{\"operator\": \"OR\"},{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"4\"}";
+                }
+                if (f5 != null) {
+                    nu.attachedTag(nu.getNews_id(), Integer.parseInt(f5));
+                    createNotiNews("", nu.getNews_id(), 500);
+                    notiFilters += ",{\"operator\": \"OR\"},{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"5\"}";
+                }
+                if (f6 != null) {
+                    nu.attachedTag(nu.getNews_id(), Integer.parseInt(f6));
+                    createNotiNews("", nu.getNews_id(), 600);
+                    notiFilters += ",{\"operator\": \"OR\"},{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"6\"}";
+                }
+                if (f7 != null) {
+                    nu.attachedTag(nu.getNews_id(), Integer.parseInt(f7));
+                    createNotiNews("", nu.getNews_id(), 700);
+                    notiFilters += ",{\"operator\": \"OR\"},{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"7\"}";
+                }
+                if (f8 != null) {
+                    nu.attachedTag(nu.getNews_id(), Integer.parseInt(f8));
+                    createNotiNews("", nu.getNews_id(), 800);
+                    notiFilters += ",{\"operator\": \"OR\"},{\"field\": \"tag\", \"key\": \"floor\", \"relation\": \"=\", \"value\": \"8\"}";
+                }
+                 notiFilters += "],";
+            }
             message = "Create News Successfully!";
-        }catch(Exception e){
+        } catch (Exception e) {
             message = "error!";
         }
         request.setAttribute("message", message);
+
+        try {
+            String jsonResponse;
+
+            URL url = new URL("https://onesignal.com/api/v1/notifications");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setUseCaches(false);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestProperty("Authorization", "Basic Zjk5OTBjMDktMjJmNi00YjY0LWI4ZjYtYTcxOTE2NmViOGIw");
+            con.setRequestMethod("POST");
+
+            String strJsonBody = "{"
+                    + "\"app_id\": \"ad05f2e7-20a3-4e8d-b8fc-006362a35cfc\","
+                    + notiFilters
+                    + "\"contents\": {\"en\": \"News: "+topic+"\"}"
+                    + "}";
+
+            System.out.println("strJsonBody:\n" + strJsonBody);
+
+            byte[] sendBytes = strJsonBody.getBytes("UTF-8");
+            con.setFixedLengthStreamingMode(sendBytes.length);
+
+            OutputStream outputStream = con.getOutputStream();
+            outputStream.write(sendBytes);
+
+            int httpResponse = con.getResponseCode();
+            System.out.println("httpResponse: " + httpResponse);
+
+            if (httpResponse >= HttpURLConnection.HTTP_OK
+                    && httpResponse < HttpURLConnection.HTTP_BAD_REQUEST) {
+                Scanner scanner = new Scanner(con.getInputStream(), "UTF-8");
+                jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+                scanner.close();
+            } else {
+                Scanner scanner = new Scanner(con.getErrorStream(), "UTF-8");
+                jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+                scanner.close();
+            }
+            System.out.println("jsonResponse:\n" + jsonResponse);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
         getServletContext().getRequestDispatcher("/createNews.jsp").forward(request, response);
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
